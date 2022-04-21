6caf93a906e49860b2d34720781ce99c26551cd0
Jaireen Singh <jaireen.s21@gmail.com>
2022-04-21 12:43:27 -0400

    starter code for node server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const app=express();
+const mongoose=require('mongoose');
+const cors=require('cors');
+
+app.use(express.json());
+app.use(express.urlencoded);
+app.use(cors());
+
+const dbUrl='mongodb://127.0.0.1:27017/order-in';
+mongoose.connect(dbUrl,{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+}).then(()=>{
+    console.log('Database connected');
+}).catch((err)=>{
+    console.log('connection error:',err);
+    process.exit(1);
+})
+const port=8010;
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`);
+})
