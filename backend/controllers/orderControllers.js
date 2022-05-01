ae920778d1d33cd60a8effeeeac058d80ffa4ce9
Jaireen Singh <jaireen.s21@gmail.com>
2022-04-30 23:04:52 -0400

    now sending email to user & resto upon order placement

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -27,19 +27,21 @@ exports.addNewOrder=catchAsync(async(req,res,next)=>{
           pass:process.env.EMAIL_PASSWORD
         },
     });
+    let date=new Date(order.date);
+    let displayDate=date.toDateString("en-CA");
     let time=order.time;
     let displayTime=  (time>12.5? ((time-12)%1===0?(time-12):(time-12.5)): (time%1===0?time:time-0.5))+":"+ (time%1===0? "00":"30") +" "+ (time>=12?"pm":"am");
-    let messageList=('<ol>');
+    let messageList="";
     for(let i=0;i<order.items.length;++i){
-        messageList+=('<li>'+ order.item.qty + 'x' + order.item.name +'</li>');
+        messageList+=('• '+ order.items[i].qty + ' x ' + order.items[i].name+'\n' );
     }
-    messageList+=('</ol>');
+   let requests=order.comments.length>0?order.comments:"--";
     const mailOptions = {
-        to: order.email,
+        to: [order.email, 'jaireen.s21@gmail.com'],//send email to user & to the restaurant email id
         from: 'jaireen.s21@gmail.com',
         subject: 'ORDER-IN : Your order has been placed',
         text: `Hello ${order.name},\n\n` +
-        `This is a confirmation email. The following order has been placed successfully:\n\n`+ `Date: ${order.date.toLocaleDatString("en-CA")}\n` + `Time: ${displayTime}\n\n` + `Method: ${order.method}\n`+ `Items:\n ${messageList}\n`+ `Total: $ ${messageList}\n\n` + 'Enjoy!\n'
+        `This is a confirmation email. The following order has been placed successfully:\n\n`+ `Method: ${order.method.toUpperCase()} | ${order.name.toUpperCase()} | ${order.phone}\n`+ `Date: ${displayDate}\n` + `Time: ${displayTime}\n\n` + `Items:\n ${messageList}`+ `TOTAL: $ ${order.total}\n\n` + `Special Requests: ${requests.toUpperCase()}\n\n`+'Enjoy!\n' + 'Team Order-In\n'
     };
     transporter.sendMail(mailOptions,(err)=>{
         if(err){
